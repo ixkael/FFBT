3ed540a772cb40a7c85fdc1d5841692a5f8c6d13
Boris Leistedt <boris.leistedt@gmail.com>
2017-03-12 18:49:10 -0400

    added roots of spherical bessel functions and discrete transform

--- ffbt/sbt.py
+++ ffbt/sbt.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from scipy.special import spherical_jn
+from scipy.special import spherical_jn, jv, jvp
+from scipy.optimize import brentq
 
 
 def continuousSphericalBesselTransform(f_r, r_finegrid, k_nodes, ell):
@@ -12,17 +13,27 @@ def continuousSphericalBesselTransform(f_r, r_finegrid, k_nodes, ell):
     return np.sqrt(2/np.pi) * flk
 
 
-def constructGridAndNorms(R, N):
-
+def constructGridAndNorms(R, N, ell):
+    # TODO: add other boundary conditions
+    roots = rootsSphericalBesselFunctions(ell+1, N)
+    qln = roots[ell, :]
+    kln_nodes = qln / R
+    cln_norms = np.sqrt(2*np.pi) / R**3 / spherical_jn(ell+1, qln)**2.
     return kln_nodes, cln_norms
 
 
-def forwardDiscreteSphericalBesselTransform(f_r, r_grid, ell, N):
+def forwardDiscreteSphericalBesselTransform(f_r, r_grid, ell, N,
+                                            kln_nodes=None, cln_norms=None):
     R = r_grid[-1]
-    # TODO: add other boundary conditions
-    kln_nodes, cln_norms = constructGridAndNorms(R, N)
-    f_l_kln = continuousSphericalBesselTransform(f_r, r_grid, kln_nodes, ell)
-    return f_l_kln, kln_nodes, cln_norms
+    if kln_nodes is None or cln_norms is None:
+        kln_nodes, cln_norms = constructGridAndNorms(R, N, ell)
+        f_l_kln = continuousSphericalBesselTransform(f_r, r_grid,
+                                                     kln_nodes, ell)
+        return f_l_kln, kln_nodes, cln_norms
+    else:
+        f_l_kln = continuousSphericalBesselTransform(f_r, r_grid,
+                                                     kln_nodes, ell)
+        return f_l_kln
 
 
 def inverseDiscreteSphericalBesselTransform(f_l_kln, klns, clns, r_grid, ell):
@@ -30,3 +41,17 @@ def inverseDiscreteSphericalBesselTransform(f_l_kln, klns, clns, r_grid, ell):
     j_ell_k_r = spherical_jn(ell, kr, derivative=False)
     f_r = np.sum(f_l_kln[None, :] * clns[None, :] * j_ell_k_r, axis=1)
     return f_r
+
+
+def rootsSphericalBesselFunctions(L, N):
+    ir = np.arange(N+1)
+    ranges = (ir + 1/2.)*np.pi - 1./(ir + 1/2.)/np.pi
+    roots = np.zeros((L, N))
+    for ell in range(L):
+        def Jn(r):
+            return spherical_jn(ell, r)
+        for j in range(N):
+            roots[ell, j] = brentq(Jn, ranges[j], ranges[j+1])
+        ranges[:-1] = roots[ell, :]
+        ranges[-1] = ranges[-2] + ranges[-2] - ranges[-3]
+    return roots
--- tests/test_sbt.py
+++ tests/test_sbt.py
@@ -3,15 +3,15 @@ import numpy as np
 from numpy.polynomial.polynomial import polyval
 import pytest
 from scipy.special import gammaln
+from scipy.special import spherical_jn
 
-from ffbt.sbt import continuousSphericalBesselTransform
-
-rtol = 1e-2
-NREPEAT = 10
+from ffbt.sbt import *
 
 
 def test_continuousSphericalBesselTransform_bettergrid():
     """Test convergence for increasingly precise grid"""
+    rtol = 1e-2
+    NREPEAT = 10
     for i in range(NREPEAT):
         P = np.random.randint(10, 20)
         coefs = np.random.uniform(0, 1, P)
@@ -32,10 +32,10 @@ def test_continuousSphericalBesselTransform_bettergrid():
         np.testing.assert_allclose(flk1, flk2, rtol=rtol)
 
 
-def test_continuousSphericalBesselTransform_analytic():
+def test_continuousSphericalBesselTransform_analytic1():
     """Test continuous SBT against analytic result"""
     rtol = 2e-2
-    NREPEAT = 5
+    NREPEAT = 2
     for i in range(NREPEAT):
         Nr = np.random.randint(50000, 100000)
         r_grid = np.logspace(-10, 3, Nr)
@@ -43,9 +43,40 @@ def test_continuousSphericalBesselTransform_analytic():
         s = np.random.uniform(-3, -2, 1)
         f_r = r_grid**s
         ell = np.random.randint(0, 3)
-        f0k = continuousSphericalBesselTransform(f_r, r_grid, k, ell)
+        flk = continuousSphericalBesselTransform(f_r, r_grid, k, ell)
         x1 = (3 + ell + s) / 2.
         x2 = (ell - s) / 2.
-        f0k_truth = 2.**(s+1.5) / k**(s+3) * np.exp(gammaln(x1) - gammaln(x2))
-        print(s, ell, f0k/f0k_truth)
-        np.testing.assert_allclose(f0k, f0k_truth, rtol=rtol)
+        flk_truth = 2.**(s+1.5) / k**(s+3) * np.exp(gammaln(x1) - gammaln(x2))
+        print(s, ell, flk/flk_truth)
+        np.testing.assert_allclose(flk, flk_truth, rtol=rtol)
+
+
+def test_rootsSphericalBesselFunctions():
+    atol = 1e-12
+    N = 100
+    L = 100
+    roots = rootsSphericalBesselFunctions(L, N)
+    ev1 = np.zeros((N, ))
+    for ell in range(L):
+        ev2 = spherical_jn(ell, roots[ell, :])
+        np.testing.assert_allclose(ev1, ev2, atol=atol)
+
+
+def test_discreteSphericalBesselTransform_loop():
+    """Test continuous SBT against analytic result"""
+    rtol = 2e-2
+    NREPEAT = 2
+    for i in range(NREPEAT):
+        R = np.random.uniform(1e0, 1e3, 1)
+        ell = np.random.randint(0, 10)
+        Nr = np.random.randint(5000, 10000)
+        N = np.random.randint(5, 100)
+        r_grid = np.logspace(-4, np.log10(R), Nr)
+        f_l_kln_t = np.random.uniform(0, 1, N)
+        kln_nodes, cln_norms = constructGridAndNorms(R, N, ell)
+        f_r = inverseDiscreteSphericalBesselTransform(
+            f_l_kln_t, kln_nodes, cln_norms, r_grid, ell)
+        f_l_kln = forwardDiscreteSphericalBesselTransform(
+            f_r, r_grid, ell, N, kln_nodes=kln_nodes, cln_norms=cln_norms)
+        print(ell, R, f_l_kln/f_l_kln_t)
+        np.testing.assert_allclose(f_l_kln_t, f_l_kln, rtol=rtol)
